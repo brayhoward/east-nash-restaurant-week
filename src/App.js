de982d3b56007ed001a63a28d8efe4352d7cff8e
brayhoward <brandonrhoward@gmail.com>
2018-01-29 17:18:41 -0600

    Layout branding section.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component, Fragment } from 'react';
-import logoSvg from './logo.svg';
 import injectSheet from 'react-jss'
+import { MED_SM } from 'constants/media-screens';
 
 const styles = {
-  logo: {
-    width: '4em'
+  appContainer: {
+    composes: 'flex flex-col flex-between full-height align-content-center',
+    margin: '1em'
+  },
+  navList: {
+    listStyle: "none"
+  },
+  branding: {
+    composes: 'bd flex flex-col flex-between justify-center align-content-center',
+    marginTop: '2rem',
+    height: '80%',
+    padding: '0 20%',
+    // Animate the breakpoint transition... b/c why not?
+    transition: 'padding 0.75s',
+    [`@media (max-width: ${MED_SM})`]: {
+      padding: '0 2%'
+    }
   }
 }
 
@@ -12,19 +27,34 @@ const styles = {
 @injectSheet(styles)
 class App extends Component {
   render() {
-    const { logo } = this.props.classes;
+    const { logo, appContainer, navList, branding } = this.props.classes;
 
     return (
-      <Fragment>
-        <header className="flex">
-          <img src={logoSvg} className={logo} alt="logo" />
-          <h1 className="App-title">East Nashville Restaruant Week</h1>
+      <div className={appContainer}>
+        {/* HEADER   */}
+        <header className="flex justify-end">
+          <ul className={navList}>
+            <li>Nav item?</li>
+          </ul>
         </header>
 
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </Fragment>
+        {/* BRANDING */}
+        <div className={branding}>
+          <div>
+            <h1 className="txt-center">
+              East Nashville Restaurant Week
+            </h1>
+
+            <p className="mg">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, iste. Quibusdam, praesentium minus!
+              Accusantium, vero minima voluptatibus reprehenderit nisi, suscipit quibusdam molestiae necessitatibus
+              tempora deserunt debitis eaque tempore officia id.  
+            </p>
+          </div>
+        </div>
+
+        {/* RESTAURANTS */}
+      </div>
     );
   }
 }
