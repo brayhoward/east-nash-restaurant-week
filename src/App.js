63157c3334cff3a6f97fc3a274adc52fb66835cf
brayhoward <brandonrhoward@gmail.com>
2018-01-29 17:16:06 -0600

    Make absolute imports work.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import logoSvg from './logo.svg';
+import injectSheet from 'react-jss'
 
+const styles = {
+  logo: {
+    width: '4em'
+  }
+}
+
+
+@injectSheet(styles)
 class App extends Component {
   render() {
+    const { logo } = this.props.classes;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <Fragment>
+        <header className="flex">
+          <img src={logoSvg} className={logo} alt="logo" />
+          <h1 className="App-title">East Nashville Restaruant Week</h1>
         </header>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </Fragment>
     );
   }
 }
