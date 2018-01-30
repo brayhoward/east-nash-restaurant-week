b5104871d1a5345fb7d33e85e0f3968279a94af4
brayhoward <brandonrhoward@gmail.com>
2018-01-29 22:00:26 -0600

    Add click handler and detail data.

--- src/layout-cmpts/PartnersSection.js
+++ src/layout-cmpts/PartnersSection.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
 import injectSheet from 'react-jss';
-import FivePoints from 'assets/five-points.jpg';
-import FivePointsDark from 'assets/five-points-dark.jpg';
-
-const partners = [
-  { name: 'five points', src: FivePoints},
-  { name: 'five points', src: FivePointsDark },
-  { name: 'five points', src: FivePoints},
-  { name: 'five points', src: FivePointsDark },
-  { name: 'five points', src: FivePoints},
-  { name: 'five points', src: FivePointsDark },
-  { name: 'five points', src: FivePoints},
-  { name: 'five points', src: FivePointsDark }
-]
+import FivePointsLogo from 'assets/five-points.jpg';
+import FivePointsDarkLogo from 'assets/five-points-dark.jpg';
+import FivePointsImg from 'assets/five-points-streetview.jpg'
 
 const styles = {
   main: {
@@ -26,6 +16,27 @@ const styles = {
 
 @injectSheet(styles)
 export default class extends Component {
+  static state = {
+    showDetail: false,
+    detail: {
+
+    }
+  }
+
+  constructor(props) { 
+    super(props);
+    const { streetView, blurb, deals } = partners[0];
+
+    this.state = {
+      showDetail: false,
+      detailCardInfo: {
+        streetView,
+        blurb,
+        deals
+      }
+    }
+  }
+
   render() {
     const { main, listItems } = this.props.classes;
 
@@ -33,9 +44,9 @@ export default class extends Component {
       <ul className={main}>
         {
           partners.map(
-            ({ src, name }, i) => (
-              <li className={listItems}>
-                <img src={src} alt={`${name} logo`} key={i} />
+            ({ logo, name, ...rest }, i) => (
+              <li className={listItems} onClick={() => this.showDetail({ ...rest })} key={i}>
+                <img src={logo} alt={`${name} logo`} />
               </li>
             )
           )
@@ -43,4 +54,94 @@ export default class extends Component {
       </ul>
     );
   }
+
+  showDetail(info) {
+    this.setState({
+      detailCardInfo: { ...info },
+      showDetail: true
+    });
+  }
 }
+
+const partners = [
+  {
+    name: 'five points',
+    logo: FivePointsLogo,
+    streetView: FivePointsImg,
+    blurb: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem expedita, alias fugit,
+      nobis ab, est laborum nemo tempora facere error quam ratione. Magnam consequatur iure minima nisi,
+      vero eligendi quae!`,
+    deals: ['1/2 off appitizers', '1/2 well drinks', '25% off entrées' ]
+
+  },
+  {
+    name: 'five points',
+    logo: FivePointsDarkLogo ,
+    streetView: FivePointsImg,
+    blurb: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem expedita, alias fugit,
+      nobis ab, est laborum nemo tempora facere error quam ratione. Magnam consequatur iure minima nisi,
+      vero eligendi quae!`,
+    deals: ['1/2 off appitizers', '1/2 well drinks', '25% off entrées' ]
+
+  },
+  {
+    name: 'five points',
+    logo: FivePointsLogo,
+    streetView: FivePointsImg,
+    blurb: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem expedita, alias fugit,
+      nobis ab, est laborum nemo tempora facere error quam ratione. Magnam consequatur iure minima nisi,
+      vero eligendi quae!`,
+    deals: ['1/2 off appitizers', '1/2 well drinks', '25% off entrées' ]
+
+  },
+  {
+    name: 'five points',
+    logo: FivePointsDarkLogo ,
+    streetView: FivePointsImg,
+    blurb: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem expedita, alias fugit,
+      nobis ab, est laborum nemo tempora facere error quam ratione. Magnam consequatur iure minima nisi,
+      vero eligendi quae!`,
+    deals: ['1/2 off appitizers', '1/2 well drinks', '25% off entrées' ]
+
+  },
+  {
+    name: 'five points',
+    logo: FivePointsLogo,
+    streetView: FivePointsImg,
+    blurb: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem expedita, alias fugit,
+      nobis ab, est laborum nemo tempora facere error quam ratione. Magnam consequatur iure minima nisi,
+      vero eligendi quae!`,
+    deals: ['1/2 off appitizers', '1/2 well drinks', '25% off entrées' ]
+
+  },
+  {
+    name: 'five points',
+    logo: FivePointsDarkLogo ,
+    streetView: FivePointsImg,
+    blurb: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem expedita, alias fugit,
+      nobis ab, est laborum nemo tempora facere error quam ratione. Magnam consequatur iure minima nisi,
+      vero eligendi quae!`,
+    deals: ['1/2 off appitizers', '1/2 well drinks', '25% off entrées' ]
+
+  },
+  {
+    name: 'five points',
+    logo: FivePointsLogo,
+    streetView: FivePointsImg,
+    blurb: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem expedita, alias fugit,
+      nobis ab, est laborum nemo tempora facere error quam ratione. Magnam consequatur iure minima nisi,
+      vero eligendi quae!`,
+    deals: ['1/2 off appitizers', '1/2 well drinks', '25% off entrées' ]
+
+  },
+  {
+    name: 'five points',
+    logo: FivePointsDarkLogo,
+    streetView: FivePointsImg,
+    blurb: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem expedita, alias fugit,
+      nobis ab, est laborum nemo tempora facere error quam ratione. Magnam consequatur iure minima nisi,
+      vero eligendi quae!`,
+    deals: ['1/2 off appitizers', '1/2 well drinks', '25% off entrées' ]
+
+   }
+]
