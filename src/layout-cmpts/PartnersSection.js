367db72ed16062588523f9c2bc7eb42751a7f183
Adam Howard <ahoward@signalinc.com>
2018-02-10 11:09:05 -0500

    added all restaurants and new favicon

--- src/layout-cmpts/PartnersSection.js
+++ src/layout-cmpts/PartnersSection.js
@@ -10,6 +10,8 @@ import ButcherImg from 'assets/Cebulka-B+B-4869.jpg';
 import MargotLogo from 'assets/logo-margot.jpg';
 import MargotImg from 'assets/ext-margot.jpg';
 import JackLogo from 'assets/logo-two-ten-jack.jpg';
+import RozeLogo from 'assets/logo-roze.jpg';
+import RozeImg from 'assets/CafeRoze-exterior.jpg';
 // import JackImg from 'assets/fort-exterior.jpg';
 import Swipeable from 'react-swipeable'
 import DetailCard from './DetailCard';
@@ -210,7 +212,6 @@ const partners = [
     map: '902 Main St',
     menu:'#',
     res:'#'
-
   },
   {
     name: 'Fort Louise',
@@ -222,7 +223,6 @@ const partners = [
     map: '1304 McGavock Pike',
     menu:'#',
     res:'https://resy.com/cities/bna/fort-louise'
-
   },
   {
     name: 'Margot',
@@ -234,7 +234,6 @@ const partners = [
     map: '1017 Woodland Street',
     menu:'',
     res:''
-    
   },
   {
     name: 'Two Ten Jack',
@@ -243,56 +242,44 @@ const partners = [
     blurb: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem expedita, alias fugit,
       nobis ab, est laborum nemo tempora facere error quam ratione. Magnam consequatur iure minima nisi,
       vero eligendi quae!`,
-    phone: '615) 454-2731',
+    phone: '(615) 454-2731',
     web: 'twotenjack.com',
     map: '1900 Eastland Ave #105',
     menu:'',
     res:''
+  },
+  {
+    name: 'Treehouse',
+    logo: 'http://www.glutenfreegeek.com/wp-content/uploads/2015/03/the-Treehouse.jpg',
+    image: 'https://pbs.twimg.com/media/CwX5zAIWgAAFeyY.jpg',
+    blurb: `Located in East Nashville’s Inglewood neighborhood, Fort Louise is a restaurant from local entrepreneur Jessica Bower and acclaimed Chicago chef Greg Biggers (McCrady’s, TRU, Morimoto) offering a friendly spin on American comfort cuisine. The space — formerly a residential house — is cozy and intimate, and there's a charming back porch for al fresco dining. Beyond the menu of snacks, salads, and large plates, the bar program boasts frozen cocktails, beers sourced from across the country, and a shareable ewer of punch called "The Test."`,
+    phone: '(615) 454-4201',
+    web: 'treehousenashville.com',
+    map: '1011 Clearview Ave',
+    menu:'',
+    res:'https://www.opentable.com/r/the-treehouse-nashville'
+  },
+  {
+    name: 'Cafe Roze',
+    logo: RozeLogo,
+    image: RozeImg,
+    blurb: `Cafe Roze, a neighborhood cafe by New York City chef Julia Jaksic, focuses on fresh, modern all day fare.  Lattes, steamers and spins on classic breakfast dishes are served throughout the mornings, salads and grain bowls come at lunchtime, and hearty globally influenced dinners with riffs on classic cocktails finish out the night. The airy minimalist space features an expansive marble bar, with pink and gray accents throughout.`,
+    phone: '(615) 645-9100',
+    web: 'caferoze.com',
+    map: '1115 Porter Rd',
+    menu:'',
+    res:'https://resy.com/cities/22/cafe-roze'
+  },
+  {
+    name: 'Peninsula',
+    logo: 'http://peninsulanashville.com/wp-content/uploads/2017/04/logo.png',
+    image: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/57022901/Peninsula.0.jpg',
+    blurb: `Cuisine of the Iberian Peninsula in the heart of East Nashville. We serve Spanish and Portuguese fare accented with French techniques, and a bar program that features a Spanish gin and tonic menu, Old World Wines, and thoughtfully crafted cocktails.`,
+    phone: '(615) 679-0377',
+    web: 'peninsulanashville.com',
+    map: '1035 W Eastland Ave',
+    menu:'',
+    res: 'https://resy.com/cities/bna/peninsula'
 
-   }
-  //,
-  // {
-  //   name: 'Treehouse',
-  //   logo: TreehouseLogo,
-  //   image: TreehouseImg,
-  //   blurb: `Located in East Nashville’s Inglewood neighborhood, Fort Louise is a restaurant from local entrepreneur Jessica Bower and acclaimed Chicago chef Greg Biggers (McCrady’s, TRU, Morimoto) offering a friendly spin on American comfort cuisine. The space — formerly a residential house — is cozy and intimate, and there's a charming back porch for al fresco dining. Beyond the menu of snacks, salads, and large plates, the bar program boasts frozen cocktails, beers sourced from across the country, and a shareable ewer of punch called "The Test."`,
-  //   phone: '',
-  //   web: '',
-  //   map: '',
-  //   menu:''
-
-  // },
-  // {
-  //   name: 'Cafe Roze',
-  //   logo: RozeLogo,
-  //   image: RozeImg,
-  //   blurb: `Located in East Nashville’s Inglewood neighborhood, Fort Louise is a restaurant from local entrepreneur Jessica Bower and acclaimed Chicago chef Greg Biggers (McCrady’s, TRU, Morimoto) offering a friendly spin on American comfort cuisine. The space — formerly a residential house — is cozy and intimate, and there's a charming back porch for al fresco dining. Beyond the menu of snacks, salads, and large plates, the bar program boasts frozen cocktails, beers sourced from across the country, and a shareable ewer of punch called "The Test."`,
-  //   phone: '',
-  //   web: '',
-  //   map: '',
-  //   menu:''
-
-  // },
-  // {
-  //   name: 'Peninsula',
-  //   logo: PeninsulaLogo,
-  //   image: PeninsulaImg,
-  //   blurb: `Located in East Nashville’s Inglewood neighborhood, Fort Louise is a restaurant from local entrepreneur Jessica Bower and acclaimed Chicago chef Greg Biggers (McCrady’s, TRU, Morimoto) offering a friendly spin on American comfort cuisine. The space — formerly a residential house — is cozy and intimate, and there's a charming back porch for al fresco dining. Beyond the menu of snacks, salads, and large plates, the bar program boasts frozen cocktails, beers sourced from across the country, and a shareable ewer of punch called "The Test."`,
-  //   phone: '',
-  //   web: '',
-  //   map: '',
-  //   menu:''
-
-  // },
-  // {
-  //   name: 'Pomodoro',
-  //   logo: PomodoroLogo,
-  //   image: PomodoroImg,
-  //   blurb: `Located in East Nashville’s Inglewood neighborhood, Fort Louise is a restaurant from local entrepreneur Jessica Bower and acclaimed Chicago chef Greg Biggers (McCrady’s, TRU, Morimoto) offering a friendly spin on American comfort cuisine. The space — formerly a residential house — is cozy and intimate, and there's a charming back porch for al fresco dining. Beyond the menu of snacks, salads, and large plates, the bar program boasts frozen cocktails, beers sourced from across the country, and a shareable ewer of punch called "The Test."`,
-  //   phone: '',
-  //   web: '',
-  //   map: '',
-  //   menu:''
-
-  // }
+  }
 ]
