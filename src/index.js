261d791aaa3ea8556e28c5e9817b88893abde613
brayhoward <brandonrhoward@gmail.com>
2018-02-10 13:13:46 -0600

    Remove service worker b/c it only works with https.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './animations.css'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import animateJumpers from './animateJumpers'
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
 animateJumpers();
